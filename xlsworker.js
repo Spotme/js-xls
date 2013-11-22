9976791460f79ed486dc668ac09fb4e09e3f88f9
Niggler <me@nig.gl>
2013-11-22 14:12:42 -0800

    web workers

--- xlsworker.js
+++ xlsworker.js
@@ -4863,3 +4863,13 @@ if(typeof module !== 'undefined' && require.main === module ) {
 	//console.log(get_formulae(ws));
 }
 })(typeof exports !== 'undefined' ? exports : XLS);
+postMessage({t:"ready"});
+
+onmessage = function (oEvent) {
+  var v, cfb;
+  try {
+    cfb = XLS.CFB.read(oEvent.data, {type:"binary"});
+    v = XLS.parse_xlscfb(cfb);
+  } catch(e) { postMessage({t:"e",d:e.stack}); }
+  postMessage({t:"xls", d:v});
+};
